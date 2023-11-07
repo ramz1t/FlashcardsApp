6cbc0b5e94f20e34dd0fbaa7d64917fe2b2355aa
Timur Ramazanov <timurram007@gmail.com>
2023-11-07 15:08:20 +0100

    Initial Commit

---
+++ FlashcardsApp/FlashcardsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsAppApp.swift
+//  FlashcardsApp
+//
+//  Created by Timur Ramazanov on 07.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
